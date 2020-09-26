ab2c5655348f969aed6b18b898cad928eb8cf765
HedleyGisbourne <hedley.gisbourne@googlemail.com>
2020-09-26 21:59:56 +0200

    Rename SpyCSSPXArb

--- Algorithm.CSharp/SpyCSSPXArb.cs
+++ Algorithm.CSharp/SpyCSSPXArb.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 
 namespace QuantConnect.Algorithm.CSharp
 {
-    public class SpyCSPXArb : QCAlgorithm
+    public class SpyCSSPXArb : QCAlgorithm
     {
         /*
         SetStartDate(2011, 01, 01);
         SetEndDate(2020, 8, 1);
         SetCash(30000);
 
-        Tickers = "CSPX", "SPY"
+        Tickers = "CSSPX", "SPY"
 
 
     Max leverage: 1.8x
@@ -128,22 +128,22 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void Initialize()
         {
-            SetStartDate(2011, 01, 01);
-            SetStartDate(2011, 01, 01);
-            SetEndDate(2020, 8, 1);
+            SetStartDate(2011, 10, 01);
+            
+            SetEndDate(2020, 9, 23);
             SetCash(30000);
             //QuantConnect.Market.AddMarkets("LSEETF", 1);
-            AddEquity("CSPX", Resolution.Daily, market: "LSEETF", extendedMarketHours: false);
+            AddEquity("CSSPX", Resolution.Daily, market: "LSEETF", extendedMarketHours: false);
             AddEquity("SPY", Resolution.Daily, market: "USA", extendedMarketHours: false);
-            AddEquity("IAUP", Resolution.Daily, market: "LSE", extendedMarketHours: false);
-
+            AddEquity("IAUP", Resolution.Daily, market: "LSEETF", extendedMarketHours: false);
+            
             //Consolidate("SPY", TimeSpan.FromMinutes(60), OnDataConsolidated);
             var market = new QuantConnect.Securities.Equity.EquityExchange();
 
             //var history = History("SPY", 250).ToArray();
             SetWarmUp(TimeSpan.FromDays(bullDays));
 
-            bullIndicator = SMA("CSPX", bullDays);
+            bullIndicator = SMA("CSSPX", bullDays);
 
             bol = new Indicators.BollingerBands(20, 2, Indicators.MovingAverageType.Exponential);
             //    bol.Update(DateTime.Now, )
@@ -157,10 +157,19 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void OnData(Slice data)
         {
-            days++;
+            if (IsWarmingUp)
+            {
+                return;
+            }
+
+            if (data.Keys.Any(k => k == "CSSPX"))
+            {
+                Console.WriteLine("GOT CSSPX");
+            }
 
             if (data.Keys.Any(k => k == "SPY"))
             {
+                Console.WriteLine($"GOT SPY {Time.DayOfWeek} {Time}");
                 var close = data.Bars["SPY"].Close;
                 if (spy.close != 0)
                 {
@@ -171,12 +180,13 @@ namespace QuantConnect.Algorithm.CSharp
 
                 if (spy.diff < -x * 2)
                 {
-                    SetHoldings("CSPX", 0);
+                    SetHoldings("CSSPX", 0);
                     // not working because the markets are not open at the same time.
                     // Copy this change to another file.
                     if (!Portfolio["IAUP"].Invested)
                     {
-                        SetHoldings("IAUP", .5m);
+                        // why only .5
+                        SetHoldings("IAUP", .5m, true);
                     }
                 }
                 else if (spy.diff > x && Time.DayOfWeek != DayOfWeek.Friday && bullIndicator.Current.Value > previousDay)
@@ -184,10 +194,9 @@ namespace QuantConnect.Algorithm.CSharp
                     // Dont get in on Monday morning
                     //  Console.WriteLine($"{previousDay} / {bullIndicator.Current.Value}");
 
-                    if (!Portfolio["CSPX"].Invested)
+                    if (!Portfolio["CSSPX"].Invested)
                     {
-
-                        SetHoldings("CSPX", 1.8);
+                        SetHoldings("CSSPX", 1.8, true);
                     }
 
                     if (Portfolio["IAUP"].Invested)
@@ -203,6 +212,13 @@ namespace QuantConnect.Algorithm.CSharp
 
         public override void OnEndOfDay()
         {
+            foreach(var p in Portfolio)
+            {
+                Console.WriteLine($"Holding {p.Key} {p.Value.Quantity}");
+            }
+
+            days++;
+
             if (Portfolio.Invested)
             {
                 investedDays++;
